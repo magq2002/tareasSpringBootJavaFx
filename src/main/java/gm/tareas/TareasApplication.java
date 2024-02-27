07c4d6ac2e4c4316f52d25bc176f824a5256b5b9
AsusMiguel <migalvez2002@gmail.com>
2024-02-26 21:37:10 -0500

    Crud completed, integrating SpringBoot and JavaFX

--- src/main/java/gm/tareas/TareasApplication.java
+++ src/main/java/gm/tareas/TareasApplication.java
@@ -1,13 +1,15 @@
 package gm.tareas;
 
-import org.springframework.boot.SpringApplication;
+import gm.tareas.presentacion.SistemasTareasFx;
+import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TareasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TareasApplication.class, args);
+//		SpringApplication.run(TareasApplication.class, args);
+		Application.launch(SistemasTareasFx.class, args);
 	}
 
 }
